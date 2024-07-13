cec7422f696d0760c3984cf69a8a405753f05c88
jguacide <guacidejohanna@gmail.com>
2024-07-13 14:40:56 +0200

    progress with MLX: window opened and pixel drawn

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: jguacide <jguacide@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 13:57:56 by jguacide          #+#    #+#             */
-/*   Updated: 2024/07/12 17:40:37 by jguacide         ###   ########.fr       */
+/*   Updated: 2024/07/13 14:40:23 by jguacide         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include "../42_lib/src/libft/libft.h"
 # include "../42_lib/src/get_next_line/get_next_line.h"
 # include "../42_lib/src/ft_printf/ft_printf.h"
+# include "../MLX42/include/MLX42/MLX42.h"
 # include <errno.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -36,11 +37,23 @@ int	exit_with_error(int n);
 
 // structs
 
-typedef struct	t_fractol
+typedef struct	s_fractol
 {
-		
+	char		*type;
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+	double		mandel_x;
+	double		mandel_y;
+	double		zoom;
+	t_color		color;
+}	t_fractol;
 
-}	s_fractol;
+typedef struct	s_color
+{
+	double	red;
+	double	green;
+	double	blue;
+}	t_color;
 
 #endif
 
--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: jguacide <jguacide@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 14:11:28 by jguacide          #+#    #+#             */
-/*   Updated: 2024/07/12 15:15:30 by jguacide         ###   ########.fr       */
+/*   Updated: 2024/07/13 14:02:47 by jguacide         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fractol.h"
+#include <stdlib.h>
 
 
 int	exit_with_error(int n)
@@ -22,14 +23,31 @@ int	exit_with_error(int n)
 
 int	main(int argc, char **argv)
 {
+	t_fractol	fractol;
+
 	if ((argc == 2 && ft_strncmp(argv[1], "mandelbrot", 11) == 0)
 		|| (argc == 2 && ft_strncmp(argv[1], "julia", 6) == 0))
 	{
-		ft_putstr_fd("Prompt OK", 1);
+		fractol.mlx = mlx_init(800, 800, argv[1], true);
+		if (!fractol.mlx)
+			exit(EXIT_FAILURE);
+		fractol.img = mlx_new_image(fractol.mlx, 800, 800);
+		if (!fractol.img)
+		{
+			printf("failed to generate image");
+			mlx_terminate(fractol.mlx);
+			exit(EXIT_FAILURE);
+		}
+		mlx_image_to_window(fractol.mlx, fractol.img, 0, 0);
+		mlx_put_pixel(fractol.img, 100, 10, 0xFF0000FF);
+		printf("pixel drawn");
+		mlx_loop(fractol.mlx);
+		mlx_terminate(fractol.mlx);
 	}
 	else
 	{
 		exit_with_error(1);
 	}
+
 	return (EXIT_SUCCESS);
 }
