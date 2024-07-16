6cc160ae3a2b62cf63ebb345df4f7e04749ab760
jguacide <guacidejohanna@gmail.com>
2024-07-16 17:16:31 +0200

    minor change with comments

--- src/keyboard.c
+++ src/keyboard.c
@@ -6,7 +6,7 @@
 /*   By: jguacide <jguacide@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 15:16:53 by jguacide          #+#    #+#             */
-/*   Updated: 2024/07/16 17:13:33 by jguacide         ###   ########.fr       */
+/*   Updated: 2024/07/16 17:16:08 by jguacide         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ void	handle_keyboard_hooks(void	*input)
 		change_type_fractol(fractol, 1);
 	else if (mlx_is_key_down(fractol->mlx, MLX_KEY_KP_2)) //TODO:check if switch to julia works fractol->position_x -= 0.1;
 		change_type_fractol(fractol, 2);
-	else if (mlx_is_key_down(fractol->mlx, MLX_KEY_KP_ADD)) //TODO: see if i can press '+'
+	else if (mlx_is_key_down(fractol->mlx, MLX_KEY_KP_ADD))
 		shift_color_palette(fractol);
 	show_fractol(fractol);
 }
